e1f6603079fd90964acdee3104550da961538d06
sutu3 <134842549+sutu3@users.noreply.github.com>
2024-11-01 13:53:42 +0700

    AddProductAPI

---
+++ src/main/java/org/example/wedservice/Controller/ProductController.java
@@ -0,0 +1,73 @@
+package org.example.wedservice.Controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.example.wedservice.Dto.Request.MaterialRequest;
+import org.example.wedservice.Dto.Request.ProductRequest;
+import org.example.wedservice.Dto.Response.ApiResponse;
+import org.example.wedservice.Dto.Response.MaterialResponse;
+import org.example.wedservice.Dto.Response.ProductResponse;
+import org.example.wedservice.Exception.AppException;
+import org.example.wedservice.Form.Material_Update;
+import org.example.wedservice.Form.Product_Update;
+import org.example.wedservice.Service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+public class ProductController {
+    ProductService productService;
+    @GetMapping
+    public ApiResponse<List<ProductResponse>> getall(){
+        return ApiResponse.<List<ProductResponse>>builder()
+                .Result(productService.getall())
+                .code(0)
+                .success(true)
+                .message("Completed")
+                .build();
+    }
+    @GetMapping("/{id}")
+    public ApiResponse<ProductResponse> getById(@PathVariable String id) throws AppException {
+        return ApiResponse.<ProductResponse>builder()
+                .Result(productService.Getbyid(id))
+                .code(0)
+                .success(true)
+                .message("Completed")
+                .build();
+    }
+    @PostMapping()
+    public ApiResponse<ProductResponse> postMaterial(@RequestBody ProductRequest request){
+        return ApiResponse.<ProductResponse>builder()
+                .Result(productService.PostProduct(request))
+                .code(0)
+                .message("Completed")
+                .success(true)
+                .build();
+    }
+    @PutMapping("/{id}")
+    public ApiResponse<ProductResponse> putMaterial(@PathVariable String id, @RequestBody Product_Update update)
+            throws AppException {
+        return ApiResponse.<ProductResponse>builder()
+                .Result(productService.PutProduct(id,update))
+                .message("Completed")
+                .code(0)
+                .success(true)
+                .build();
+    }
+    @DeleteMapping("/{id}")
+    public ApiResponse<Void> deleteCategory(@PathVariable String id) throws AppException {
+        productService.DeleteProduct(id);
+        return ApiResponse.<Void>builder()
+               .message("Delete Completed")
+               .code(0)
+               .success(true)
+               .build();
+    }
+}
